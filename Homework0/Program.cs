5c636e2b183b2cdb188e792303eb88a472f8f859
Nkita <kablovnv@gmail.com>
2023-02-24 18:39:07 +0300

    создал папку для дз

---
+++ Homework0/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear(); 
+Console.Write("a: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("b: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("c: ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (b > max) {
+    max = b;
+ }
+if (c > max) 
+{
+   max = c;
+}
+Console.WriteLine($"максимальное число: {max}"); 
+
+
+  
+
